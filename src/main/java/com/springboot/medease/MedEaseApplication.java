346455db9efa1ad2c2eb6bcf4fdb42150d82fe84
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-12-01 12:44:00 +0200

    Initial commit

---
+++ src/main/java/com/springboot/medease/MedEaseApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.medease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedEaseApplication.class, args);
+    }
+
+}
