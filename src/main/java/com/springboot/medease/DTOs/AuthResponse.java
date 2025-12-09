a810e05022d59cc3af42024ba222a212fc8f1f07
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-12-09 17:07:46 +0200

    Refactor: changed the jwtUtils to use the usetype instead of role

--- src/main/java/com/springboot/medease/DTOs/AuthResponse.java
+++ src/main/java/com/springboot/medease/DTOs/AuthResponse.java
@@ -1,5 +1,6 @@
 package com.springboot.medease.DTOs;
 
+import com.springboot.medease.Models.UserType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ public class AuthResponse {
     private String message;
     private String userId;
     private String identifier;
-    private String role;
+    private UserType userType;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private String token;
