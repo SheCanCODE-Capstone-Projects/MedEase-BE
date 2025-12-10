e4a10c92130be8e94fe381e202d32cee292753a0
Marie23560 <kanzayiremariepascale@gmail.com>
2025-12-10 14:31:06 +0200

    Refactor:  added the size constraint on the email

--- src/main/java/com/springboot/medease/DTOs/PatientUpdateRequest.java
+++ src/main/java/com/springboot/medease/DTOs/PatientUpdateRequest.java
@@ -19,19 +19,27 @@ public class PatientUpdateRequest {
 
         @Size(max = 50, message = "First name must not exceed 50 characters")
         private String firstName;
+
         @Size(max = 50, message = "Last name must not exceed 50 characters")
         private String lastName;
+
         @Email(message = "Email must be valid")
+        @Size(max = 255, message = "Email must not exceed 255 characters")
         private String email;
+
         @Size(max = 15, message = "Phone number must not exceed 15 characters")
         private String phoneNumber;
+
         private Date dateOfBirth;
+
         private Gender gender;
 
         @Size(max = 100, message = "Insurance provider must not exceed 100 characters")
         private String insuranceProvider;
+
         @Size(max = 50, message = "Insurance number must not exceed 50 characters")
         private String insuranceNumber;
+
         @Size(max = 1001, message = "Subjective must not exceed 1000 characters")
         private String subjective;
     }
