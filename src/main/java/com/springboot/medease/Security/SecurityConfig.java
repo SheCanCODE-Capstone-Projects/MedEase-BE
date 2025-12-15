92c7bc134af1de694769cf0f8908bcdc428f29aa
nadineirakoze5 <nirakoze72@gmail.com>
2025-12-15 14:54:44 +0200

    fix(security): permit /api/auth/google/login endpoint

--- src/main/java/com/springboot/medease/Security/SecurityConfig.java
+++ src/main/java/com/springboot/medease/Security/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
 
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/pharmacist/register").permitAll()
+                        .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/pharmacist/register","/api/auth/google/login").permitAll()
                         .requestMatchers("/api/pharmacy/**").hasRole("PHARMACIST")
                         .requestMatchers("/api/patient/**").hasRole("PATIENT")
                         .requestMatchers("/api/doctor/**").hasRole("DOCTOR")
